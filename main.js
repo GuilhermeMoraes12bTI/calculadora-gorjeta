85d3e2ebc3174dd3cf594cb60c967c12be24c323
guilhermemoraesrodrigues2116 <guilhermemoraesrodrigues2116@gmail.com>
2025-03-30 16:47:01 +0000

    criação da lógica do botão Reset e fim do projeto

--- main.js
+++ main.js
@@ -40,15 +40,7 @@ function receiveTipPercentage (value){
 function receiveCustomTipPercentage (){
     let customTipInput = document.querySelector("#custom-tip")
     tipPercentage = customTipInput.valueAsNumber / 100
-
-    if (buttonSelected !== null){
-
-        buttonSelected.classList.remove("button-selected")
-        buttonSelected = null
-    }
-
-   
-
+   removeButtonSelected()
     calculate()
 }
 
@@ -59,18 +51,37 @@ function calculate(){
 
        let  tipAmountStrong = document.querySelector(".amount strong")
        let tipAmountPerson = (bill *  tipPercentage) / numberOfPeople
-       tipAmountStrong.innerText = `$ ${ tipAmountPerson.toFixed(2)}`
+       tipAmountStrong.innerText = `$${ tipAmountPerson.toFixed(2)}`
 
        let totalStrong = document.querySelector(".total strong")
        let total = (bill / numberOfPeople) + tipAmountPerson
 
-       totalStrong.innerText = `$ ${total.toFixed(2)}`
+       totalStrong.innerText = `$${total.toFixed(2)}`
 
     } else {
         console.log ("Ainda não é possível calcular")
     }
 }
 
-function reset (){
-    
+function reset() {
+
+    billInput.value = ""
+    bill = 0
+
+    numberOfPeopleInput.value = ""
+    numberOfPeople = 0 
+    removeButtonSelected()
+    document.querySelector("#custom-tip").value = ""
+    tipPercentage = 0
+    document.querySelector(".amount strong").innerText = '$0.00'
+    document.querySelector(".total strong").innerText = 
+'0.00'
+}
+
+function removeButtonSelected(){
+    if (buttonSelected !== null){
+
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
 }
